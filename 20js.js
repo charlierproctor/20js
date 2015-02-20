9dc363b58530dd7afe06db09a0d28f2bcdce97a3
charlierproctor <charlie.proctor@yahoo.com>
2015-02-19 19:54:30 -0500

    can just use their filtering system to order desc

--- 20js.js
+++ 20js.js
@@ -1,16 +1,10 @@
 var request = require('request'), express = require('express'), app = express();
 app.get('/', function(req,res){
-	request('https://data.ny.gov/resource/6amx-2pbv.json', function (error,response,body) {
+	request('https://data.ny.gov/resource/6amx-2pbv.json?$order=aadt DESC', function (error,response,body) {
 	  if (!error && response.statusCode == 200) {
 	    var h, json = JSON.parse(body);
-		res.send(max(json));
+		res.send(json[0]);
 	  }
 	})
 });
 app.listen(process.env.PORT || 3000);
-
-function max(json){ var max = 0;
-	for (var i = 0; i < json.length; i++){ 
-		if (json[i].aadt > max) { max = json[i]; }
-	} return max;
-}
