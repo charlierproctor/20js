eb28be9c7ac522f5b721b37a53a73d6e02a92567
charlierproctor <charlie.proctor@yahoo.com>
2015-02-19 21:52:56 -0500

    adding option to show all... only 13 LOC

--- 20js.js
+++ 20js.js
@@ -1,18 +1,13 @@
-var request = require('request'), express = require('express'), app = express();
-app.get('/:zip', function(req,res){
-request('https://health.data.ny.gov/api/views/cnih-y5dw/rows.json', 
-function (error,response,body) {
-var s = "<head>\n<style>html, body, #map-canvas {height: 100%;margin: 0px;"
-s+="padding: 0px}</style>\n<script src='https://maps.googleapis.com/maps/api/"
-s+="js?v=3.exp'></script>\n<script>\nfunction initialize(){ \nvar mapOptions ="
-s+=" { zoom: 7, center: new google.maps.LatLng(43, -74)}; \nvar map = new "
-s+="google.maps.Map(document.getElementById('map-canvas'),mapOptions);\n"
-var e="} \ngoogle.maps.event.addDomListener(window, 'load', initialize);"
-e+="\n</script>\n</head>\n<body>\n<div id='map-canvas'>\n</div>\n</body>"
-res.send(s + JSON.parse(body).data.filter(function(elem){ 
-if(elem[20] && elem[13]){ 
-return (elem[20].trim() === req.params.zip)&&(elem[12]!=="0")}
-else {return false }})
-.map(function(elem){return "new google.maps.Marker({position: new google.maps.\
-LatLng(" + elem[34][1] + "," + elem[34][2] + "),map: map,title: \""+ elem[8]
-.trim() + "\"});\n"}).join(" ")+e)})}); app.listen(process.env.PORT || 3000);
+var app = require('express')(); app.get('/:z?', function(req,res){
+require('request')('https://health.data.ny.gov/api/views/cnih-y5dw/rows.json', 
+function (e,r,b) {res.send("<head>\n<style>html, body, #canv {height: \
+100%;margin: 0px;padding: 0px}</style>\n<script src='https://maps.googleapis.co\
+m/maps/api/js?v=3.exp'></script>\n<script>\nfunction i(){ \nvar opt={ zoom: 7, \
+center: new google.maps.LatLng(43,-74)};\nvar map=new google.maps.Map(document\
+.getElementById('canv'),opt);\n"+JSON.parse(b).data.filter(function(x){ 
+if(x[20] && x[13]){return (req.params.z?(x[20].trim()===req.params.z):1)
+&&(x[12]!=="0")}else {return false }}).map(function(x){return "new google.maps\
+.Marker({position: new google.maps.LatLng(" + x[34][1] + "," + x[34][2] + "),\
+map: map,title: \""+ x[8].trim().replace(/"/g, '\\\"')+"\"});\n"}).join(" ")+
+"}google.maps.event.addDomListener(window,'load',i);</script>\n</head>\n<body>\
+\n<div id=\'canv'></div></body>"); }); });app.listen(process.env.PORT || 3000);
