85a4f9c8f4c47a10580e256e9926c1dc0d610094
charlierproctor <charlie.proctor@yahoo.com>
2015-02-19 20:27:29 -0500

    find food inspection results by zip code for NYC

--- 20js.js
+++ 20js.js
@@ -1,9 +1,14 @@
 var request = require('request'), express = require('express'), app = express();
 app.get('/', function(req,res){
-	request('https://data.ny.gov/resource/6amx-2pbv.json?$order=aadt DESC', function (error,response,body) {
+	request('https://health.data.ny.gov/api/views/cnih-y5dw/rows.json', function (error,response,body) {
 	  if (!error && response.statusCode == 200) {
 	    var h, json = JSON.parse(body);
-		res.send(json[0]);
+	    res.send(json.data.filter(function(elem){
+	    	if(elem[20]){ 
+	    		console.log(elem);
+	    		return elem[20].trim() == '12953'; 
+	    	} else { return false }
+	    }))
 	  }
 	})
 });
